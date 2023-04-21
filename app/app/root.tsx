f18d5025028a0ba5c75286baed2ab31e52f4e5aa
Andres6936 <andres6936@gmail.com>
2023-04-21 15:44:42 -0500

    Added Master CSS

--- app/app/root.tsx
+++ app/app/root.tsx
@@ -1,27 +1,35 @@
 import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
+    Links,
+    LiveReload,
+    Meta,
+    Outlet,
+    Scripts,
+    ScrollRestoration,
 } from "@remix-run/react";
+import {MasterCSS} from '@master/css'
+import config from '../master.css'
+import {useLayoutEffect} from "react";
+
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+    useLayoutEffect(() => {
+        new MasterCSS(config)
+    })
+
+    return (
+        <html lang="en">
+        <head>
+            <meta charSet="utf-8"/>
+            <meta name="viewport" content="width=device-width,initial-scale=1"/>
+            <Meta/>
+            <Links/>
+        </head>
+        <body>
+        <Outlet/>
+        <ScrollRestoration/>
+        <Scripts/>
+        <LiveReload/>
+        </body>
+        </html>
+    );
 }
