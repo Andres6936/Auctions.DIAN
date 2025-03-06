27fe73deef9614b73783b736d892de850d9ed48d
Andres6936 <andres6936@gmail.com>
2025-03-06 15:45:26 -0500

    Remove APITester component and redesign App layout
    The APITester component was removed, and the App layout was restructured to display a detailed profile view, utilizing the newly added `ChevronLeft` and `ListIcon` components. Migrated styles to use `@master/normal.css` for simplicity, and additional dependencies like `react-router` were introduced for enhanced routing capabilities.

--- login/src/App.tsx
+++ login/src/App.tsx
@@ -1,24 +1,53 @@
 import "./index.css";
-import { APITester } from "./APITester";
-
-import logo from "./logo.svg";
-import reactLogo from "./react.svg";
+import photo from '@/img/KevinSpace.jpg';
+import {ChevronLeft} from "@/icons/ChevronLeft";
+import {ListIcon} from "@/icons/List";
 
 export function App() {
   return (
-    <div className="app">
-      <div className="logo-container">
-        <img src={logo} alt="Bun Logo" className="logo bun-logo" />
-        <img src={reactLogo} alt="React Logo" className="logo react-logo" />
-      </div>
+      <main className="flex flex:col bg:slate-95 font:sans">
+          <div className="position:relative flex:1">
+              <img className="w:100%" src={photo} alt="Photo"/>
 
-      <h1>Bun + React</h1>
-      <p>
-        Edit <code>src/App.tsx</code> and save to test HMR
-      </p>
-      <APITester />
-    </div>
+              <div className="position:absolute top:15 left:15 py:0.8rem px:1rem bg:rgba(255,255,255,0.2) bd:blur(12px) r:0.5rem">
+                  <ChevronLeft className={"fg:white"}/>
+              </div>
+
+              <div className="position:absolute top:15 right:15 py:0.8rem px:1rem bg:rgba(255,255,255,0.2) bd:blur(12px) r:0.5rem">
+                  <ListIcon className={"fg:white"}/>
+              </div>
+          </div>
+
+          <section className="px:1.5rem r:2rem flex flex:col translateY(-3rem) bg:slate-95">
+              <h1 className="font-size:2rem font:sans mb:0">Kevin Space</h1>
+              <h4 className="font-size:1.2rem opacity:0.5 font-weight:medium font:sans mt:0">President of America</h4>
+
+              <div className="flex flex:col mt:1rem mb:2rem gap-y:0.5rem">
+                  <div className="flex flex:col">
+                      <p className={"m:0 opacity:0.8"}>Biologic Gender</p>
+                      <p className={"m:0 font-size:1.3rem font-weight:bold"}>Masculine</p>
+                  </div>
+
+                  <div className="flex flex:row gap-x:2rem">
+                      <div className="flex flex:col">
+                          <p className={"m:0 opacity:0.8"}>Date Born</p>
+                          <p className={"m:0 font-size:1.3rem font-weight:bold"}>1965/04/02</p>
+                      </div>
+
+                      <div className="flex flex:col">
+                          <p className={"m:0 opacity:0.8"}>Start Date</p>
+                          <p className={"m:0 font-size:1.3rem font-weight:bold"}>2001/05/14</p>
+                      </div>
+                  </div>
+
+                  <div className="flex flex:col">
+                      <p className={"m:0 opacity:0.8"}>Salary</p>
+                      <p className={"m:0 font-size:1.3rem font-weight:bold"}>$ 1,200.0</p>
+                  </div>
+              </div>
+
+              <button className="bg:blue-50 fg:white py:0.5rem font-size:1L.2rem r:1rem b:none">Modify</button>
+          </section>
+      </main>
   );
 }
-
-export default App;
