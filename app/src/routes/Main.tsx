f520a97a3b1f26e301f3d0e0485bcde868545bec
Andres6936 <andres6936@gmail.com>
2025-03-06 19:57:43 -0500

    Add interactive Dock component with animations
    Introduced a new Dock component using Framer Motion for responsive animations and expanded navigation functionality. The Dock is integrated into the Main route, featuring interactive icons with hover effects and tooltips. Updated dependencies to include "motion" and "lucide-react".

--- app/src/routes/Main.tsx
+++ app/src/routes/Main.tsx
@@ -1,7 +1,22 @@
+import Dock from "@/components/dock";
+import {Home, Archive, User, Settings} from "lucide-react";
+
 export function Main() {
+    const items = [
+        {icon: <Home size={18} className="color:white"/>, label: 'Home', onClick: () => alert('Home!')},
+        {icon: <Archive size={18} className="color:white"/>, label: 'Archive', onClick: () => alert('Archive!')},
+        {icon: <User size={18} className="color:white"/>, label: 'Profile', onClick: () => alert('Profile!')},
+        {icon: <Settings size={18} className="color:white"/>, label: 'Settings', onClick: () => alert('Settings!')},
+    ];
+
     return (
         <section className="flex flex:1 flex:col h:100vh max-h:100vh w:100vw max-w:100vw bg:slate-95 font:sans">
-            <p>Main</p>
+            <Dock
+                items={items}
+                panelHeight={68}
+                baseItemSize={50}
+                magnification={70}
+            />
         </section>
     )
 }
