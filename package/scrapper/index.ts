76aaf41bee986db48d9e672b71f5e99e2252e1a2
Andres6936 <andres6936@gmail.com>
2025-03-07 00:29:54 -0500

    Add scrapper logic for fetching and storing auctions
    Introduce a scrapper module that authenticates, retrieves auctions data, and stores it in a SQLite database. An environment variable for the base URL was added, and adjustments were made to `.gitignore` to allow `.env` inclusion. This foundational setup enables scraping from a specific API endpoint.

--- package/scrapper/index.ts
+++ package/scrapper/index.ts
@@ -1,1 +1,43 @@
-console.log("Hello via Bun!");
+import { Database } from "bun:sqlite";
+
+const db = new Database("Auctions.sqlite");
+
+db.query(`
+    CREATE TABLE IF NOT EXISTS Auctions (
+        "Serial" INTEGER PRIMARY KEY AUTOINCREMENT,
+        "Payload" TEXT NOT NULL
+    )
+`).run();
+
+const useQuery = async (url: string, options?: RequestInit) => {
+    return await fetch(new URL(url, process.env.BASE_URL).href, options);
+}
+
+const getToken = async () => {
+    const stream = await useQuery("/remate-virtual/api/auth/signin", {
+        method: "POST",
+        body: JSON.stringify({
+            usernameOrEmail: "EnEtldJjmen8BQk8XlbS6Ua5f7W3MhQ5lMmOEnIWyxM=",
+            password: "JWG4oaaul1oX+fxYdUDO/r7HPcxjwMPnRDHfgf2oy5I="
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    const payload = await stream.json();
+    return payload.accessToken;
+}
+
+const token = await getToken();
+const streamAuctions = await useQuery("/remate-virtual/api/v1/revautos/list/0/25", {
+    headers: {
+        Authorization: `Bearer ${token}`
+    }
+})
+
+console.log("Auctions:")
+const auctions = await streamAuctions.json();
+for (const auction of auctions) {
+    console.log("Inserting value of auction")
+    db.query(`INSERT INTO Auctions ("Payload") VALUES ('${JSON.stringify(auction)}')`).run()
+}
