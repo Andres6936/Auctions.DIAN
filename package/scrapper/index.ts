04e8a3eb19bb5ab7eadd505e88150710c36c0452
Andres6936 <andres6936@gmail.com>
2025-03-14 22:49:04 -0500

    Add data insertion logic for Autos table in scrapper
    Implemented parsing and validation of auction payloads to extract data and insert it into the Autos table. Added checks for payload content and error handling to ensure robust processing. Limited operation to 10 entries per execution for safety.

--- package/scrapper/index.ts
+++ package/scrapper/index.ts
@@ -1,9 +1,60 @@
 import {Database} from "bun:sqlite";
 import { drizzle } from "drizzle-orm/bun-sqlite";
+import {Autos} from "./src/db/schema.ts";
 
 const sqlite = new Database(process.env.DB_FILE_NAME!);
 const db = drizzle({client: sqlite});
 
 (async () => {
+    let i = 0;
+    const query = sqlite.query("SELECT * FROM Auctions");
+    for (const row of query.iterate()) {
+        try {
+            const payload = JSON.parse(row.Payload)
+            if (!(payload instanceof Array)) {
+                console.error("The payload is not an array, skipping...")
+                continue;
+            }
 
+            if (payload.length === 0) {
+                console.error("The payload is empty, skipping...")
+                continue;
+            }
+
+            if (payload.length >= 2) {
+                console.error("The payload has more than one element, skipping...")
+                continue;
+            }
+
+            const object = payload[0];
+
+            await db.insert(Autos).values({
+                IdAuto: object.idAuto,
+                AutoNumber: object.nroAuto,
+                DelegationResolutionNumber: object.nroResolucionDelegacion,
+                DelegationResolutionDate: object.fechaResolucionDelegacion,
+                AutoDate: object.fechaAuto,
+                FileNumber: object.nroExpedente,
+                SectionalAddress: object.direccionSeccional,
+                Dependency: object.dependencia,
+                AutoDescription: object.descripcionAuto,
+                Commissioner: object.comisionado,
+                BiddingPercentage: object.porcentajeLicitacion,
+                TotalBiddingBaseValue: object.valorTotalBaseLicitacion,
+                BidPercentage: object.porcentajePostura,
+                BidValue: object.valorPostura,
+                DepositAccountNumber: object.nroCuentaDeposito,
+                TotalAppraisalValue: object.valorTotalAvaluos,
+                CreatedBy: object.creadoPor,
+                CreationDate: object.fechaCreacion,
+                ModifiedBy: object.modificadoPor,
+                ModificationDate: object.fechaModificacion,
+            })
+        } catch (e) {
+            console.error('Error parsing payload, caused by ', e)
+        }
+
+        i += 1;
+        if (i > 10) break;
+    }
 })()
