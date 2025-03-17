adccc60a23014fe24aaf93fa15226f77842b12a4
Andres6936 <andres6936@gmail.com>
2025-03-16 19:42:21 -0500

    Add image processing with Sharp and update test handling
    Integrated Sharp for image compression and processing within the scrapper module. Updated tests to write images with varied quality and added Sharp as a dependency. Adjusted related locks and configurations to ensure compatibility.

--- package/scrapper/index.test.ts
+++ package/scrapper/index.test.ts
@@ -1,6 +1,7 @@
 import {getToken, useQuery} from "./src/login.ts";
 
 import { S3Client } from "bun";
+import sharp from "sharp";
 
 const minio = new S3Client({
     accessKeyId: process.env.MINIO_ACCESS_KEY,
@@ -18,7 +19,7 @@ const minio = new S3Client({
         method: "POST",
         body: JSON.stringify({
             nroRadicado: 52631376171995,
-            usuarioSistema: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDIxNzExMzEsInN1YiI6Ijk5OS0xIiwiYXVkIjpbIldvMGFLQWxCN3ZSUF8xNmZyUEkxeDlacGhCRWEiXSwiaXNzIjoiaHR0cHM6XC9cL211aXNjYS5kaWFuLmdvdi5jbyIsImp0aSI6IjU1NTE3OTEzLTg4NDgtNGY1ZS04YmFiLWQ3YmU0YjkyYmQzZiIsImlhdCI6MTc0MjE2NzUzMX0.nb5U8W-KhhxmOUfzLBBTt9dBilSQ6CPZAmV0ezOe7dI",
+            usuarioSistema: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDIxNzU2MDcsInN1YiI6Ijk5OS0xIiwiYXVkIjpbIldvMGFLQWxCN3ZSUF8xNmZyUEkxeDlacGhCRWEiXSwiaXNzIjoiaHR0cHM6XC9cL211aXNjYS5kaWFuLmdvdi5jbyIsImp0aSI6IjUwNTE4NDVjLWEzNTktNDE5Yy1iM2NjLWU1NGYyZWYzN2FjOSIsImlhdCI6MTc0MjE3MjAwN30._n8Xf1UgXZuk6M5jiw67eAmcNU-THbnvBC85K8qVUdI",
         }),
         headers: {
             "Content-Type": "application/json",
@@ -28,5 +29,6 @@ const minio = new S3Client({
 
     const response = await stream.json();
     const buffer = Buffer.from(response.body, 'base64');
-    await minio.write('A.jpeg', buffer);
+    await Bun.write('A-Original.jpeg', buffer);
+    await sharp(buffer).jpeg({quality: 80}).toFile('A-Quality-80.jpeg');
 })()
