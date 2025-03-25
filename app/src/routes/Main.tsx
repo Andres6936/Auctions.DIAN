999f9285691b5fcdfb3425400c5cddf7ce4c4c9d
Andres6936 <andres6936@gmail.com>
2025-03-24 21:59:54 -0500

    Add property search form and update audience display
    Implemented a new property search form with multiple input fields and search functionality in the Main component. Updated the layout to display upcoming auction details and adjusted styles with a custom font and CSS improvements. This enhances the user experience and navigation within the application.

--- app/src/routes/Main.tsx
+++ app/src/routes/Main.tsx
@@ -1,7 +1,7 @@
 import Dock from "@/components/dock";
 import {Archive, Home, Settings, User} from "lucide-react";
 import {useNavigate} from "react-router";
-import {UserButton, useUser} from "@stackframe/react";
+import {useUser} from "@stackframe/react";
 
 export function Main() {
     useUser({ or: "redirect" });
@@ -29,7 +29,78 @@ export function Main() {
 
     return (
         <section className="flex flex:1 flex:col h:100vh max-h:100vh w:100vw max-w:100vw bg:slate-95 font:sans">
-            <UserButton/>
+            <div className="flex flex:col bg:#003566 p:1.5rem py:4rem gap:2rem">
+                <h1 className="color:white text:center font:1.5rem font:bold">Encuentra bienes en remate</h1>
+
+                <div className="flex flex:col p:1rem bg:white r:0.5rem">
+                    <label>
+                        Tipo de bien:
+                        <input type="text"/>
+                    </label>
+
+                    <label>
+                        Tipo de inmueble:
+                        <input type="text"/>
+                    </label>
+
+                    <label>
+                        Departamento:
+                        <input type="text"/>
+                    </label>
+
+                    <label>
+                        Ciudad:
+                        <input type="text"/>
+                    </label>
+
+                    <label>
+                        Valor desde:
+                        <input type="text"/>
+                    </label>
+
+                    <label>
+                        Valor hasta:
+                        <input type="text"/>
+                    </label>
+
+                    <label>
+                        Fecha inicial audiencia:
+                        <input type="date"/>
+                    </label>
+
+                    <label>
+                        Fecha final audiencia:
+                        <input type="date"/>
+                    </label>
+                </div>
+
+                <p>Buscar por palabra</p>
+
+                <button>Buscar</button>
+            </div>
+
+            <h2>Proximas audiencias</h2>
+
+            <div>
+                <div className="bg:red w:100 video"/>
+                <p>Número del remate</p>
+                <p>202506300000085</p>
+
+                <p>Yopal - Casanare</p>
+                <p>Fecha de audiencia</p>
+                <p>2025-03-25</p>
+
+                <p>Hora audiencia</p>
+                <p>10:00</p>
+
+                <p>Avaluó del bien:</p>
+                <p>922,140,000 COP</p>
+
+                <p>Valor base oferta</p>
+                <p>645,498,000 COP</p>
+
+                <button>Ver</button>
+            </div>
 
             <Dock
                 items={items}
