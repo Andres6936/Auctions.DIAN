70bd7fe27c3cccf7426eae7628df22af0fb57084
Andres6936 <andres6936@gmail.com>
2025-04-01 10:39:47 -0500

    Remove unnecessary TypeScript file extension in import
    Eliminated the `.ts` extension from an import statement to conform with standard import practices. This change ensures consistency and prevents potential issues in module resolution.

--- packages/schemas/src/query/autos.ts
+++ packages/schemas/src/query/autos.ts
@@ -1,7 +1,7 @@
 import {db} from '../server/db.client';
 import {Autos} from "../db/schema";
 import {desc, eq} from "drizzle-orm";
-import {DEFAULT_PAGE, DEFAULT_PAGE_SIZE} from "../defaults/const.ts";
+import {DEFAULT_PAGE, DEFAULT_PAGE_SIZE} from "../defaults/const";
 
 export class AutosQuery {
     public static async getAll(page = DEFAULT_PAGE, pageSize = DEFAULT_PAGE_SIZE) {
