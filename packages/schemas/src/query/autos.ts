6e0ff41b0bcb4a94acbff36bf6a1cdf9fb4f1a0b
Andres6936 <andres6936@gmail.com>
2025-04-01 10:36:51 -0500

    Add pagination support to AutosQuery.getAll method
    Introduced DEFAULT_PAGE and DEFAULT_PAGE_SIZE constants for managing pagination defaults. Updated the getAll method in AutosQuery to support pagination with configurable page and page size parameters.

--- packages/schemas/src/query/autos.ts
+++ packages/schemas/src/query/autos.ts
@@ -1,9 +1,13 @@
 import {db} from '../server/db.client';
 import {Autos} from "../db/schema";
 import {desc} from "drizzle-orm";
+import {DEFAULT_PAGE, DEFAULT_PAGE_SIZE} from "../defaults/const.ts";
 
 export class AutosQuery {
-    public static async getAll() {
-        return db.select().from(Autos).limit(10).orderBy(desc(Autos.CreationDate))
+    public static async getAll(page = DEFAULT_PAGE, pageSize = DEFAULT_PAGE_SIZE) {
+        return db.select().from(Autos)
+            .orderBy(desc(Autos.CreationDate), desc(Autos.IdAuto))
+            .limit(pageSize)
+            .offset((page - 1) * pageSize)
     }
 }
