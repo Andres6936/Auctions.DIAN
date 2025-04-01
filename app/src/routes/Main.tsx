a3501096984de157462db566eb0688b613f3ce97
Andres6936 <andres6936@gmail.com>
2025-04-01 10:23:00 -0500

    Add 'key' prop to Auction component and refine UI structure
    Add a unique 'key' prop to the Auction component to improve React performance. Adjust the Auction component's UI layout for better readability and maintainability by reorganizing and nesting the structure. These changes enhance both functionality and code clarity.

--- app/src/routes/Main.tsx
+++ app/src/routes/Main.tsx
@@ -58,7 +58,7 @@ export function Main() {
 
                 <div className="grid grid-cols:3@md grid-cols:4@3xl gap:1.5rem">
                     {query.data && query.data.body.Items.map((auction) =>
-                        <Auction model={auction}/>
+                        <Auction key={auction.IdAuto} model={auction}/>
                     )}
                 </div>
             </div>
@@ -75,7 +75,7 @@ export function Main() {
 
 function Auction({model}: { model: AuctionModel }) {
     return (
-        <div className="flex flex:col gap:0.7rem p:1.5rem bg:#f8f9fa border:1px|solid|#e9ecef r:0.5rem">
+        <div className="flex flex:col gap:0.7rem bg:#f8f9fa border:1px|solid|#e9ecef r:0.5rem">
             <div className="rel bg:#dee2e6 w:full video r:0.5rem">
                 <div className="abs top:0 left:0 bottom:0 right:0 w:full h:full r:0.5rem">
                     {model.Images && model.Images.length > 0 && (
@@ -88,38 +88,40 @@ function Auction({model}: { model: AuctionModel }) {
                 </div>
             </div>
 
-            <div className="flex flex:row gap-x:0.5rem bg:#7de2d1 b:1px|solid|#00cecb p:0.3rem r:0.3rem">
-                <Text size={Text.Size.S}>Número del remate:</Text>
-                <Text size={Text.Size.S} bold>{model.AutoNumber}</Text>
-            </div>
+            <div className="flex flex:col gap:0.7rem p:1rem">
+                <div className="flex flex:row gap-x:0.5rem bg:#7de2d1 b:1px|solid|#00cecb p:0.3rem r:0.3rem">
+                    <Text size={Text.Size.S}>Número del remate:</Text>
+                    <Text size={Text.Size.S} bold>{model.AutoNumber}</Text>
+                </div>
 
-            <Text size={Text.Size.S} info>YOPAL - CASANARE</Text>
+                <Text size={Text.Size.S} info>YOPAL - CASANARE</Text>
 
-            <div>
-                <div className="flex flex:row gap-x:2rem">
-                    <div className="flex flex:col">
-                        <Text size={Text.Size.S} bold>Fecha de audiencia</Text>
-                        <Text size={Text.Size.M}>2025-03-25</Text>
-                    </div>
+                <div>
+                    <div className="flex flex:row gap-x:2rem">
+                        <div className="flex flex:col">
+                            <Text size={Text.Size.S} bold>Fecha de audiencia</Text>
+                            <Text size={Text.Size.M}>2025-03-25</Text>
+                        </div>
 
-                    <div className="flex flex:col">
-                        <Text size={Text.Size.S} bold>Hora audiencia</Text>
-                        <Text size={Text.Size.M}>10:00</Text>
+                        <div className="flex flex:col">
+                            <Text size={Text.Size.S} bold>Hora audiencia</Text>
+                            <Text size={Text.Size.M}>10:00</Text>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div className="flex flex:row gap-x:0.5rem">
-                <Text size={Text.Size.S} bold>Avaluó del bien:</Text>
-                <Text size={Text.Size.S} bold>{formatMoney(model.TotalAppraisalValue || 0)} COP</Text>
-            </div>
+                <div className="flex flex:row gap-x:0.5rem">
+                    <Text size={Text.Size.S} bold>Avaluó del bien:</Text>
+                    <Text size={Text.Size.S} bold>{formatMoney(model.TotalAppraisalValue || 0)} COP</Text>
+                </div>
 
-            <div className="flex flex:col mb:0.5rem">
-                <Text size={Text.Size.S} info>Valor base oferta</Text>
-                <Text bold>{formatMoney(model.TotalBiddingBaseValue || 0)} COP</Text>
-            </div>
+                <div className="flex flex:col mb:0.5rem">
+                    <Text size={Text.Size.S} info>Valor base oferta</Text>
+                    <Text bold>{formatMoney(model.TotalBiddingBaseValue || 0)} COP</Text>
+                </div>
 
-            <Button success>Ver</Button>
+                <Button success>Ver</Button>
+            </div>
         </div>
     )
 }
