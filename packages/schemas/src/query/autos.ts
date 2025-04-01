27dc100a9c1c48835c21f6ea77f4872afdae4d43
Andres6936 <andres6936@gmail.com>
2025-04-01 10:39:22 -0500

    Refactor getAll method to include subquery with inner join
    Introduced a subquery to optimize data selection and applied an inner join for better filtering based on Auto IDs. This enhances query structure and ensures results are consistently ordered by creation date.

--- packages/schemas/src/query/autos.ts
+++ packages/schemas/src/query/autos.ts
@@ -1,13 +1,20 @@
 import {db} from '../server/db.client';
 import {Autos} from "../db/schema";
-import {desc} from "drizzle-orm";
+import {desc, eq} from "drizzle-orm";
 import {DEFAULT_PAGE, DEFAULT_PAGE_SIZE} from "../defaults/const.ts";
 
 export class AutosQuery {
     public static async getAll(page = DEFAULT_PAGE, pageSize = DEFAULT_PAGE_SIZE) {
-        return db.select().from(Autos)
+        const subQuery = db.select({Id: Autos.IdAuto})
+            .from(Autos)
             .orderBy(desc(Autos.CreationDate), desc(Autos.IdAuto))
             .limit(pageSize)
             .offset((page - 1) * pageSize)
+            .as('SubQuery')
+
+        return db.select()
+            .from(Autos)
+            .innerJoin(subQuery, eq(Autos.IdAuto, subQuery.Id))
+            .orderBy(desc(Autos.CreationDate))
     }
 }
