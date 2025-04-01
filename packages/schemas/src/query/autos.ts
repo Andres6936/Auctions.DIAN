84b19e91553663c3270fb8a1f5ec3e52f68b8309
Andres6936 <andres6936@gmail.com>
2025-04-01 10:31:19 -0500

    Sort autos by creation date in descending order
    Updated the getAll query to include an orderBy clause, sorting autos by their CreationDate in descending order. This ensures that the most recently created autos appear first in the result set.

--- packages/schemas/src/query/autos.ts
+++ packages/schemas/src/query/autos.ts
@@ -1,8 +1,9 @@
 import {db} from '../server/db.client';
 import {Autos} from "../db/schema";
+import {desc} from "drizzle-orm";
 
 export class AutosQuery {
     public static async getAll() {
-        return db.select().from(Autos).limit(10)
+        return db.select().from(Autos).limit(10).orderBy(desc(Autos.CreationDate))
     }
 }
