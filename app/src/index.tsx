79dd7c3984596d1bea5d8106e0c664ae75b63e3a
Andres6936 <andres6936@gmail.com>
2025-07-20 16:40:20 -0500

    Enable HMR and console log proxying in development mode

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -69,7 +69,13 @@ const server = serve({
         },
     },
 
-    development: process.env.NODE_ENV !== "production",
+    development: process.env.NODE_ENV !== "production" && {
+        // Enable browser hot reloading in development
+        hmr: true,
+
+        // Echo console logs from the browser to the server
+        console: true,
+    },
 });
 
 console.log(`🚀 Server running at ${server.url}`);
