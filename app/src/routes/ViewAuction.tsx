b2e9d8316baa2628b87c01144ba0339d93bd41b7
Andres6936 <andres6936@gmail.com>
2025-07-20 17:57:36 -0500

    Add auction details section to ViewAuction route with Mantine Button for offers

--- app/src/routes/ViewAuction.tsx
+++ app/src/routes/ViewAuction.tsx
@@ -1,12 +1,65 @@
 import {Fragment} from "react";
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
+import {Button} from "@mantine/core";
 
 export default function Page() {
     return (
         <Fragment>
             <Header/>
+            <section className="flex flex:col gap:1rem px:3rem py:3rem bg:white">
+                <div>
+                    <div className="flex flex:row align-items:center justify-content:space-between font:1.2rem">
+                        <p>Número del Remate: <span className="font:bold">2025060100468</span></p>
+                        <p className="font:0.95rem">Fecha de publicación del aviso del remate: <span
+                            className="font:bold">11/7/2025</span></p>
+                    </div>
+                    <div>
+                        <p className="opacity:0.7 font:0.95rem">POPAYÁN - CAUCA</p>
+                    </div>
+                </div>
 
+                <div className="flex flex:row gap:1rem mt:3rem">
+                    <div
+                        className="rel flex flex:3 align-items:center justify-content:center border:1px|solid|gray-80 r:10 p:1rem">
+                        <div className="flex w:25rem square bg:gray-80 r:10"></div>
+
+                        <div
+                            className="abs top:0 right:0 p:1rem bg:gray-90 rtr:10 rbl:20 text:white font:bold text-align:center">
+                            <p>70%</p>
+                        </div>
+                    </div>
+
+                    <div className="flex flex:2 flex:col gap:1rem">
+                        <div className="flex flex:col gap:1rem border:1px|solid|gray-80 r:10 p:1rem">
+                            <div className="flex flex:row justify-content:space-between">
+                                <div className="flex flex:col">
+                                    <p>Fecha de Audiencia</p>
+                                    <p className="font:bold">22/7/2025</p>
+                                </div>
+                                <div className="flex flex:col">
+                                    <p>Hora de Audiencia</p>
+                                    <p className="font:bold">10:00</p>
+                                </div>
+                            </div>
+
+                            <p>Avalúo del bien: <span className="font:bold">$18,400,000</span></p>
+
+                            <div>
+                                <p>Valor base oferta</p>
+                                <p className="font:bold">$12,880,000</p>
+                            </div>
+                        </div>
+
+                        <div className="flex flex:col gap:1rem border:1px|solid|gray-80 r:10 p:1rem">
+                            <p className="font:bold">Quiero hacer una oferta para este remate</p>
+                            <Button>
+                                Ingresar
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </section>
             <Footer/>
         </Fragment>
     )
